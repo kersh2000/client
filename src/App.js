1d0cc3a47853a7ebc9aa04d51329aa511622474e
kersh2000 <bradleykershaw333@gmail.com>
2023-06-08 01:38:31 +0100

    Fixed return user data bug

--- src/App.js
+++ src/App.js
@@ -1,34 +1,29 @@
 import './App.css';
 import { useEffect, useState } from 'react';
-
 import axios from 'axios';
 
 function App() {
-
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
     axios.get('https://cloud-project-server.onrender.com/users')
       .then(res => {
-        setUsers(res.data)
-      }).catch( error => {
+        setUsers(res.data);
+      })
+      .catch(error => {
         console.error(error);
       });
   }, []);
 
-  console.log(users);
-
   return (
     <div className="App">
       <h1>User App</h1>
-      {
-        users.map(user => {
-          <div key={user.id}>
-            <h2>{user.username}</h2>
-            <h3>{user.password}</h3>
-          </div>
-        })
-      }
+      {users.map(user => (
+        <div key={user.id}>
+          <h2>{user.username}</h2>
+          <h3>{user.password}</h3>
+        </div>
+      ))}
     </div>
   );
 }
