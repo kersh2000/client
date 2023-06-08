1f27cc678d5929d41782bacf562d7f265e5ad0a6
kersh2000 <bradleykershaw333@gmail.com>
2023-06-08 01:03:28 +0100

    Added axios and get request

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import './App.css';
+import { useEffect, useState } from 'react';
+
+import axios from 'axios';
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get('/users')
+      .then(res => {
+        setUsers(res.data)
+      }).catch( error => {
+        console.error(error);
+      });
+  }, []);
+
   return (
     <div className="App">
-      
+      <h1>User App</h1>
+
+      {
+        users.map(user => {
+          <div key={user.id}>
+            <h2>{user.username}</h2>
+            <h3>{user.password}</h3>
+          </div>
+        })
+      }
     </div>
   );
 }
